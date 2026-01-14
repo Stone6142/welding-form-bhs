6f6d50778ad836ef20b30695164ca04be44142c8
Sky B. <120291635+Stone6142@users.noreply.github.com>
2026-01-14 15:01:30 +0000

    Rollback - Hosting issues

--- client/firebase.js
+++ client/firebase.js
@@ -3,22 +3,25 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-async function loadFirebaseConfig() {
-  const res = await fetch("/api/firebaseConfig");
-  return await res.json();
-}
-
-loadFirebaseConfig().then((config) => {
-  const app = initializeApp(config);
-    // Initialize Firebase
-    const auth = getAuth(app);
-    const db = getDatabase(app);
-    // Expose to window so other scripts can use them
-    window.firebaseApp = app;
-    window.firebaseAuth = auth;
-    window.firebaseDb = db;
-});
-
+// TODO: Replace with your real Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyBkUu-N7nkDkclV5SkDmozK2SmSur0NWNQ",
+    authDomain: "welding-form.firebaseapp.com",
+    databaseURL: "https://welding-form-default-rtdb.firebaseio.com",
+    projectId: "welding-form",
+    storageBucket: "welding-form.firebasestorage.app",
+    messagingSenderId: "69271735442",
+    appId: "1:69271735442:web:e438afe73f604563d22a4b",
+    measurementId: "G-JSR34FQ59Y"
+  };
   
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+// Expose to window so other scripts can use them
+window.firebaseApp = app;
+window.firebaseAuth = auth;
+window.firebaseDb = db;
 
